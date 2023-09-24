cb1eaf48d30630d2b552edf1f2cbe4ce762d7018
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2023-09-25 07:21:22 +0900

    update

--- rand/rand.js
+++ rand/rand.js
@@ -21,3 +21,27 @@ function calculatePercent(input) {
     return output;
 }
 
+
+/**
+ * @param {BigInt} input
+ * @returns Number
+ */
+function calculatePercentFloat(input) {
+    let output = input >> BigInt(32);
+    output = output * BigInt(100);
+    let test = (BigInt(2) << BigInt(31));
+    //output = output / test;
+    output = division(output, test);
+    return output;
+}
+
+/**
+ * Thanks https://stackoverflow.com/questions/54409854/how-to-divide-two-native-javascript-bigints-and-get-a-decimal-result
+ *
+ * @param {BigInt} a
+ * @param {BigInt} b
+ * @returns Number
+ */
+function division(a, b){
+    return Number(a * 10000n / b) / 10000;
+}
