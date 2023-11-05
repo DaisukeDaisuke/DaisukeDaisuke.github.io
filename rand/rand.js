c32a4b8d5952fb8765f626861d5709da50a2cc22
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2023-11-06 06:48:26 +0900

    a

--- rand/rand.js
+++ rand/rand.js
@@ -54,3 +54,48 @@ function getRand(nowPercent, max){
     let randomValue = nowPercent / 100 * max;
     return Math.floor(randomValue);
 }
+
+/**
+ * @param {BigInt} seed - input seed
+ */
+function ARand(seed){
+    let before = seed;
+    seed = seed * BigInt("0x41C64E6D");
+    seed = seed + BigInt("0x3039");
+    seed = seed & BigInt("0xFFFFFFFF");
+
+    let ret = BigInt("0x7FFF") & rightShift(seed,0x10);
+    return [seed, ret];
+}
+
+/**
+ * @param {BigInt} seed - input seed
+ * @return BigInt
+ */
+function getType(seed){
+    let seed1 = rightShift(seed, 0x1f);
+    seed = ((leftShift(seed, 0x1d) & BigInt("0xFFFFFFFF"))) - seed1 & BigInt("0xFFFFFFFF");
+    if (seed < 0){
+        seed = BigInt("0xFFFFFFFF") - seed;
+    }
+    seed1 = seed1 + ror(seed, BigInt(0x1d));
+    seed1 = seed1 & BigInt(0xff);
+    return seed1;
+}
+
+/**
+ * @param {BigInt} input - input seed
+ * @param {BigInt} ror
+ * @return BigInt
+ */
+function ror(input, ror){
+    return (rightShift(input, ror) | (leftShift(input, (BigInt(32) - ror)))) & BigInt(0xFFFFFFFF);
+}
+
+function leftShift(num, shift) { // <<
+    return num * (2n ** BigInt(shift));
+}
+
+function rightShift(num, shift) { // >>
+    return num / (2n ** BigInt(shift));
+}
