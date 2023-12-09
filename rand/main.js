f80ab7f28e10873aac0d3420a074f56c3324f2ef
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2023-12-09 11:00:52 +0900

    main.js

--- rand/main.js
+++ rand/main.js
@@ -41,6 +41,48 @@ function generateDropdown() {
     dropdownCounter++;
 }
 
+function createBrElement(parent){
+    // 新しいp要素を作成
+    const newParagraph = document.createElement('br');
+
+    // 親要素を取得
+    const parentElement = document.getElementById(parent);
+
+    // 新しいp要素を親要素に追加
+    parentElement.appendChild(newParagraph);
+}
+
+function createPElement(parent, content){
+    // 新しいp要素を作成
+    const newParagraph = document.createElement('p');
+
+    // 新しいp要素のテキストコンテンツを設定（ここでは検索ワードとして"新しい要素"を使用）
+    newParagraph.textContent = content;
+
+    // 親要素を取得
+    const parentElement = document.getElementById(parent);
+
+    // 新しいp要素を親要素に追加
+    parentElement.appendChild(newParagraph);
+}
+
+function createCheckbox(containerId, id, labelText, isChecked) {
+    const parentElement = document.getElementById(containerId);
+    // 新しいチェックボックスを作成
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = isChecked;
+    checkbox.id = id;
+
+    // ラベルを作成
+    var label = document.createElement('label');
+
+    label.appendChild(document.createTextNode(labelText));
+
+    // 親要素に追加
+    parentElement.appendChild(label);
+    parentElement.appendChild(checkbox);
+}
 
 function createSelectElement(containerId, label, options) {
     // コンテナ要素を取得
@@ -78,4 +120,20 @@ function generateEmptyArray(rows, columns, start) {
     }
 
     return resultArray;
+}
+
+function checkArrayContains(inputArray, targetArray) {
+    // 入力と目標の各要素が一致するか確認
+    for (let targetItem of targetArray) {
+        // いずれかの入力要素が目標要素と一致すればtrueを返す
+        if (inputArray.some(inputItem => inputItem.includes(targetItem))) {
+            continue;
+        } else {
+            // 一致しない場合はfalseを返す
+            return false;
+        }
+    }
+
+    // 全ての目標要素が一致した場合はtrueを返す
+    return true;
 }
